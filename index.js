5495cef88090da824643ef6a65bc8bc6100c96b2
praneeth724 <tmkpraneeth@std.appsc.sab.ac.lk>
2025-08-12 20:26:14 +0530

    test

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ const app = express();
 app.get("/", (req, res) => res.json({ ok: true }));
 
 const port = process.env.PORT || 8080; // <-- important
-app.listen(port, () => console.log("listening on", port));
+app.listen(port, () => console.log("listening on test", port));
